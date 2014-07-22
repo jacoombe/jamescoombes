aeed0db47e119efa462e8936b13a233787f65540
James Coombes <james@Jamess-MacBook-Air.local>
2014-07-22 13:13:12 +0100

    Moved some stuff about mainly the views into a new file

---
+++ app/views.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from app import app
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))    
