4823c44246619810610b84de3554322954e090e1
James Coombes <james@jamess-air.corp.velocix.com>
2014-07-23 14:14:05 +0100

    This is the initial template work and removed the config.py file as it shouldnt be visible to the world

--- config.py
+++
@@ -1,3 +0,0 @@
-DEBUG = True # Turns on debugging features in Flask
-BCRYPT_LEVEL = 12 # Configuration for the Flask-Bcrypt extension
-MAIL_FROM_EMAIL = "jamespjcoombes@gmail.com" # For use in application emails
