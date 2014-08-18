5418f1f7adb4c00ff14770c5e31db87c8f45b965
James Coombes <james@Jamess-MacBook-Air.local>
2014-08-18 20:02:11 +0100

    This is the first part of the login work we need to fix the unittests to run now

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object('config')
+from app import views
+app.config.from_object('app.configuration.DevConfig')
 
 # Now we can access the configuration variables via app.config["VAR_NAME"].
